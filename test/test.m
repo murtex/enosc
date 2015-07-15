6c45928860f88031af53dcc1ca209735037850c1
murtex <kuberski@uni-potsdam.de>
2015-07-15 13:25:40 +0200

    added matlab test

---
+++ test/test.m
@@ -0,0 +1,20 @@
+function test( infile, outdir )
+% test example
+%
+% TEST( infile, outdir )
+%
+% INPUT
+% infile : data filename (row char)
+% outdir : output directory (row char)
+
+		% safeguard
+	if nargin < 1 || ~isrow( infile ) || ~ischar( infile )
+		error( 'invalid argument: infile' );
+	end
+
+	if nargin < 2 || ~isrow( outdir ) || ~ischar( outdir )
+		error( 'invalid argument: outdir' );
+	end
+
+end
+
