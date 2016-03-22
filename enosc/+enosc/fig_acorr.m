546130d874fcda5feee7fbd06c73a2e9e227e673
murtex <kuberski@uni-potsdam.de>
2016-03-22 21:46:13 +0100

    updated plot

--- enosc/+enosc/fig_acorr.m
+++ enosc/+enosc/fig_acorr.m
@@ -78,9 +78,9 @@ function fig_acorr( h5c, times, epsilons, betas, plotfile )
 		'defaultAxesXGrid', 'on', 'defaultAxesYGrid', 'on' ...
 		);
 
-	semilogx( adxdt, 'Color', style.color( 'neutral', +2 ), 'DisplayName', 'oscillator' );
-	hold on
 	semilogx( awn, 'Color', style.color( 'neutral', 0 ), 'DisplayName', 'white noise' );
+	hold on
+	semilogx( adxdt, 'Color', style.color( 'neutral', +2 ), 'DisplayName', 'oscillator' );
 	%semilogx( aorder, 'Color', style.color( 'neutral', +2 ), 'DisplayName', 'order parameter' );
 
 	title( sprintf( 'frequency autocorrelation (epsilon: %s, beta/pi: %s)', enosc.par2str( epsilons ), enosc.par2str( betas ) ) );
